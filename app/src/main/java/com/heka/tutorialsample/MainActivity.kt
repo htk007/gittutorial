e328476882896a6c96c05e4587bc635068bcd61e
htk007 <hasankaya921@hotmail.com>
2022-10-23 13:39:57 +0300

    second commit

--- app/src/main/java/com/heka/tutorialsample/MainActivity.kt
+++ app/src/main/java/com/heka/tutorialsample/MainActivity.kt
@@ -8,4 +8,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    //this is second commit for example
 }
